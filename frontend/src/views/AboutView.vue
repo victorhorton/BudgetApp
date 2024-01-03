bc5a9872356fd0412c113d06c6fe207f568456fa
Victor Horton <vichorton5@gmail.com>
2024-01-02 19:25:24 -0600

    Switch to vue frontend

---
+++ frontend/src/views/AboutView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+  <ul class="list-group">
+    <li
+      class="list-group-item"
+      v-for="weatherItem in weather"
+      :key="weatherItem.date"
+    >
+      {{ weatherItem }}
+    </li>
+  </ul>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "AboutView",
+  data() {
+    return {
+      weather: [],
+    };
+  },
+  mounted() {
+    axios({
+      method: "get",
+      url: "http://localhost:5286/WeatherForecast",
+      responseType: "json",
+    }).then((resp) => {
+      this.weather = resp.data;
+    });
+  },
+};
+</script>
