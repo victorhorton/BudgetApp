8f90b69a76ef35af7135bde12fb511e8e498c583
Victor Horton <vichorton5@gmail.com>
2024-01-03 18:50:46 -0600

    Move navbar into component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,19 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <NavBar></NavBar>
   <router-view />
 </template>
+
+<script>
+// @ is an alias to /src
+import NavBar from "@/components/NavBar.vue";
+
+export default {
+  name: "App",
+  components: {
+    NavBar
+  }
+};
+</script>
 
 <style lang="scss">
 #app {
---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,12 @@
+<template>
+  <nav>
+    <router-link to="/" active-class="active">Home</router-link> |
+    <router-link to="/about" active-class="active">About</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavBar"
+};
+</script>
