15a63b0b6c09fcfab2a4e0eaf33bec4bdc40d26c
Victor Horton <vichorton5@gmail.com>
2024-01-03 19:01:38 -0600

    Switch navbar to use BS5 component

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,7 +1,35 @@
 <template>
-  <nav>
-    <router-link to="/" active-class="active">Home</router-link> |
-    <router-link to="/about" active-class="active">About</router-link>
+  <nav class="navbar navbar-expand-lg">
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-bs-toggle="offcanvas"
+      data-bs-target="#navbarOffcanvasLg"
+      aria-controls="navbarOffcanvasLg"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div
+      class="offcanvas offcanvas-top"
+      tabindex="-1"
+      id="navbarOffcanvasLg"
+      aria-labelledby="navbarOffcanvasLgLabel"
+    >
+      <div class="offcanvas-body">
+        <ul class="navbar-nav justify-content-center flex-grow-1 pe-3">
+          <li class="nav-item">
+            <router-link to="/" active-class="active" class="nav-link"
+              >Home</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link to="/about" active-class="active" class="nav-link"
+              >About</router-link
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
   </nav>
 </template>
 
@@ -10,3 +38,13 @@ export default {
   name: "NavBar"
 };
 </script>
+
+<style>
+.navbar-toggler {
+  border: none !important;
+
+  &:focus {
+    box-shadow: none !important;
+  }
+}
+</style>
