f12fd31ff267c695d89162574cced0a91e1eee5f
Victor Horton <vichorton5@gmail.com>
2024-01-03 18:45:26 -0600

    Start clean frontend vue app

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,20 +3,8 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <NavBar />
   <router-view />
 </template>
-
-<script>
-import NavBar from "@/components/NavBar.vue";
-
-export default {
-  name: "App",
-  components: {
-    NavBar,
-  },
-};
-</script>
 
 <style lang="scss">
 #app {
--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -2,35 +2,4 @@
   <div class="about">
     <h1>This is an about page</h1>
   </div>
-  <ul class="list-group">
-    <li
-      class="list-group-item"
-      v-for="weatherItem in weather"
-      :key="weatherItem.date"
-    >
-      {{ weatherItem }}
-    </li>
-  </ul>
 </template>
-
-<script>
-import axios from "axios";
-
-export default {
-  name: "AboutView",
-  data() {
-    return {
-      weather: [],
-    };
-  },
-  mounted() {
-    axios({
-      method: "get",
-      url: "http://localhost:5286/Budget",
-      responseType: "json",
-    }).then((resp) => {
-      this.weather = resp.data;
-    });
-  },
-};
-</script>
